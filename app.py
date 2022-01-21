c7d07a5196480082c305a5b5947582b4f272a8c0
ilyas gedikoğlu <igedikoglugs@hotmail.com>
2022-01-21 16:11:37 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World"
